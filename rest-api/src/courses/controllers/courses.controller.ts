d8d651f58b6e9561c68d688d0f2db8ab08331479
Gabriel Rockson <grockson.ext@igus.net>
2024-01-23 16:10:03 +0000

    refactor: move courses controller to dedicated folder

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { Course } from '../../../shared/course';
-import { findAllCourses } from '../../db-data';
+import { Course } from '../../../../shared/course';
+import { findAllCourses } from '../../../db-data';
 
 @Controller('api')
 export class CoursesController {
--- rest-api/src/courses/courses.module.ts
+++ rest-api/src/courses/courses.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { CoursesController } from './courses.controller';
+import { CoursesController } from './controllers/courses.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoursesSchema } from './schema/courses.schema';
 
